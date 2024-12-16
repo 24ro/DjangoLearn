dbd41386b1d7850bae3fdd82465edc0c42430c51
24ro <Mussasarodrigue@gmail.com>
2024-12-16 08:14:30 -0500

    database & admin panel

--- djlearn/base/admin.py
+++ djlearn/base/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
 
 # Register your models here.
+from .models import Room, Topic, Message
+
+try:
+    admin.site.register(Room)
+except AlreadyRegistered:
+    pass
+try:
+    admin.site.register(Topic)
+except AlreadyRegistered:
+    pass
+try:
+    admin.site.register(Message)
+except AlreadyRegistered:
+    pass
